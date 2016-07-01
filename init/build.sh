6fed34ef9754d0989b85413ae40c7293f446fd2f
Emilio Norrmann <enorrmann@gmail.com>
2016-07-01 17:45:09 +0000

    links

--- init/build.sh
+++ init/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ROO_HOME=/home/enorrmann/alter_dev/spring-roo-1.3.2.RELEASE/bin
+ROO_HOME=/home/ubuntu/workspace/spring-roo-1.3.2.RELEASE/bin
 BACKUP_FOLDER=init
 $ROO_HOME/roo.sh script $BACKUP_FOLDER/backup.roo
 cp $BACKUP_FOLDER/messages_es.properties  src/main/resources/ar/com/ix/proyectos/web/i18n
