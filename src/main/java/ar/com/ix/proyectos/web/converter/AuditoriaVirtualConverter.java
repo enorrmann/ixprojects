d66d85746b463b99b91a74f7248fe6f542e4860c
Emilio Norrmann <enorrmann@santafe.gov.ar>
2017-01-23 12:26:11 -0300

    rebase

--- src/main/java/ar/com/ix/proyectos/web/converter/AuditoriaVirtualConverter.java
+++
@@ -1,8 +0,0 @@
-package ar.com.ix.proyectos.web.converter;
-import ar.com.ix.proyectos.model.AuditoriaVirtual;
-import javax.faces.convert.Converter;
-import org.springframework.roo.addon.jsf.converter.RooJsfConverter;
-
-@RooJsfConverter(entity = AuditoriaVirtual.class)
-public class AuditoriaVirtualConverter implements Converter {
-}
